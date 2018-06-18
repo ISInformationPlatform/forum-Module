4b13cf2f9defc1c2f32fc19734a66c8c64559bf1
evi0s <1417213864@qq.com>
2018-06-18 15:20:12 +0800

    Update tests

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var url = "mongodb://mongo:27017/";
 
-var mongo = require('../lib/mongo.js')(url);
+var mongo = require('../src/mongo.js')(url);
 
 var database = "test";
 var collection = "test";
@@ -17,3 +17,5 @@ mongo.find(database, collection, findObj1, function(err, result){
   console.log(mongo.ObjectId2UnixTimeStamp(result[0]._id));
   console.log(typeof mongo.String2ObjectId((result[0]._id).toString()));
 });
+
+console.log(mongo.String2ObjectId());
