e36521b9ff361e6b4b19c8e595c27ccb4f1ddb0b
hwfhc <gyczt879574764@gmail.com>
2018-07-30 16:16:01 +0800

    debug

--- test/common.js
+++ test/common.js
@@ -1,5 +1,5 @@
 module.exports = {
-    URL: 'mongodb://www.lrworkshop.xin:27017/',
+    URL: 'mongodb://localhost:27017/',
     DATABASE: 'ISInformationPlatform',
     POST_COLLECTION: 'postlist_1',
     COMMENT_COLLECTION: 'postcomment_1'
